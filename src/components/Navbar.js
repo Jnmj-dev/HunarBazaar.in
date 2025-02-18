fad9ef5ac3b3ba1345da2c790966ea4f1b160341
Sagar Thakur <sagar_cst24@delhitechnicalcampus.ac.in>
2025-02-19 00:10:39 +0530

    Home Page changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,18 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          HunarBazaar
+          <span className="brand-hunar">Hunar</span>
+          <span className="brand-bazaar">Bazaar</span>
         </Link>
         <div className="navbar-links">
           <Link to="/" className="navbar-link">Home</Link>
           <Link to="/marketplace" className="navbar-link">Marketplace</Link>
           <Link to="/worker-profile" className="navbar-link">Workers</Link>
-          <Link to="/post-job" className="navbar-button">Post a Job</Link>
+          <Link to="/post-job" className="navbar-link">Post a Job</Link>
+        </div>
+        <div className="navbar-auth">
+          <Link to="/login" className="navbar-button login">Login</Link>
+          <Link to="/signup" className="navbar-button signup">Sign Up</Link>
         </div>
       </div>
     </nav>
